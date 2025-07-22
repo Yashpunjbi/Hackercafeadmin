1eef59bb27edc4ee8b458d626a84f04ade01a054
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 16:11:00 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,22 @@
+import { createContext, useContext, useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = async (email, password) => {
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    setUser(res.user);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
