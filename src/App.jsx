d4431e51ad1c2cc679f22f924ed5cf5bc1331d4f
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-22 16:14:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./pages/Home";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Login />;
+};
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default App;
