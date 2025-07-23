f279ea58720d4cceab960bba504483f6fcfbe13a
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-24 00:42:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import Home from "./pages/Home";
+import Orders from "./components/Orders";
 
+const App = () => {
+  return (
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 bg-gray-100 min-h-screen">
+          <Topbar />
+          <div className="p-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
