bdc99f1bdf08d91e767ecff8d3260618d8aaeaa0
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-07-26 13:00:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC00vdY1ePvGb6eIq3GYTn9sZgHQm0lqGc",
   authDomain: "ddoskitchen.firebaseapp.com",
   projectId: "ddoskitchen",
-  storageBucket: "ddoskitchen.firebasestorage.app",
+  storageBucket: "ddoskitchen.appspot.com",
   messagingSenderId: "57929834946",
   appId: "1:57929834946:web:3975dcb77bbe7bb1d5765b",
   measurementId: "G-V49H346Q22"
