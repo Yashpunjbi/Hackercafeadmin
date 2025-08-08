78326d7b801ded8388624a54a4636804083cd98a
Yashpunjbi <128663439+Yashpunjbi@users.noreply.github.com>
2025-08-08 20:39:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-
+import Layout from "./Layout";
 import Dashboard from "./components/Dashboard";
 import Orders from "./components/Orders";
 import Products from "./components/Products";
@@ -10,27 +8,21 @@ import Offers from "./components/Offers";
 import Banners from "./components/Banners";
 import Categories from "./components/Categories";
 import PromoCodes from "./components/PromoCodes";
-import Layout from './Layout';
+
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Sidebar />
-
-        <div className="md:ml-64 pt-16 p-4">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/offers" element={<Offers />} />
-            <Route path="/banners" element={<Banners />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/promocodes" element={<PromoCodes />} />
-<Route path="/" element={<Layout />}>
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="products" element={<Products />} />
+          <Route path="offers" element={<Offers />} />
+          <Route path="banners" element={<Banners />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="promocodes" element={<PromoCodes />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
